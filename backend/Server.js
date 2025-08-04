ecc8b54d42e3311c5116da5aeb064259617f1c0a
Absar Ahmad <siddiqui.mr10@gmail.com>
2025-08-04 19:35:27 +0530

    changes for deployment

--- backend/Server.js
+++ backend/Server.js
@@ -15,7 +15,14 @@ connectCloudinary();
 
 // middlewares
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 // api endpoints
 
