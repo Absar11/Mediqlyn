0357eb71fa666de3c5cb6eddd21f7de1bf891c40
Absar Ahmad <121477562+Absar11@users.noreply.github.com>
2026-06-26 20:19:38 +0530

    Add 'atoken' to allowedHeaders in CORS configuration

--- backend/Server.js
+++ backend/Server.js
@@ -26,7 +26,7 @@ app.use(
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization", "token"],
+    allowedHeaders: ["Content-Type", "Authorization", "token", "atoken"],
   })
 );
 
